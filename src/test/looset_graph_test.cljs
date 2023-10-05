b69bd3e810ebb722f94f5cdef7a32a4b0cd412e0
João Paulo <joao.paulo@vouch.io>
2023-10-05 17:15:30 -0300

    Test

--- src/test/looset_graph_test.cljs
+++ src/test/looset_graph_test.cljs
@@ -4,11 +4,6 @@
     [looset-graph.app :as app]
     [looset-graph.graph-parser :as graph-parser]))
 
-(deftest descriptions-example
-  (testing "testing description"
-    (is (= 1 2)
-        "'Should be' description")))
-
 (deftest nodes-with-no-parent
   (is (= {"label1" {"node5" {}, "node1" {}, "node2" {}},
           "label2" {"node5" {}, "soAndOnLabel2" {}},
@@ -44,6 +39,25 @@
            (graph-parser/graph-ast)
            ;; (#(do (tap> {:ast %}) %))
            (app/graph-ast->nodes-map)
+           (#(do (tap> {:nodes-map %}) %))
+           (app/nodes-hierarchy)))))
+           ;; (#(do (tap> {:nodes-hierarchy-from-test %}) %))))))
+
+(deftest node-and-label
+  (is (= {"label1" {"node1" {}, "node2" {}},
+          "node3" {"node4" {}, "node5" {}}}
+         (->>"=>label1:
+                node1
+                node2;
+
+              node3:
+                node4
+                node5;
+
+              node1 -> node2;"
+           (graph-parser/graph-ast)
+           ;; (#(do (tap> {:ast %}) %))
+           (app/graph-ast->nodes-map)
            ;; (#(do (tap> {:nodes-map %}) %))
            (app/nodes-hierarchy)))))
-           ;; (#(do (tap> {:nodes-hierarchy-from-test %}) %))
+           ;; (#(do (tap> {:nodes-hierarchy-from-test %}) %))))))
