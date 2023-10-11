673910da900a58a40cfd7f1c41e81ebdf31b06ad
João Paulo <joao.paulo@vouch.io>
2023-10-11 10:06:14 -0300

    Split the lix and label style. Lix won't be bold

--- src/main/looset_graph/app.cljs
+++ src/main/looset_graph/app.cljs
@@ -520,11 +520,13 @@
       [:path {:d "M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"}]]]]])
 
 (defn node-view
-  [{{:keys [color]} :style
+  [{:keys [class]
+    {:keys [color]} :style
     {:keys [level hidden? path node-id]} :node}
    text]
-  [:div.node-item
+  [:div
    {:style {:paddingLeft (+ 16 (* 12 level))}
+    :class class
     :onMouseOver #(>evt [::node-hovered node-id])
     :onMouseOut #(>evt [::node-hovered nil])}
    [:span.hover-gray
@@ -540,13 +542,15 @@
 (defn label-node [{:keys [node-id color opened?] :as node-item}]
   [node-view
    {:node node-item
+    :class "label-style"
     :style {:color color}}
    (str (if opened? "=v " "=> ")
         node-id)])
 
 (defn lix-node [{:keys [node-id opened?] :as node-item}]
   [node-view
-   {:node node-item}
+   {:node node-item
+    :class "lix-style"}
    (str (cond (nil? opened?) ""
               (true? opened?) "v "
               :else "> ")
@@ -630,7 +634,15 @@
      background-color: #7c7c7c;
    }
 
-   .node-item {
+   .lix-style {
+     display: flex;
+     flex-direction: row;
+     font-family: "label-font-family";
+     font-size: large;
+     padding-bottom: 13px;
+   }
+
+   .label-style {
      display: flex;
      flex-direction: row;
      font-family: "label-font-family";
