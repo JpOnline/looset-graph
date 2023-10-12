50d8162c52e4770d451ae75494869f26629632f6
João Paulo <joao.paulo@vouch.io>
2023-10-12 17:09:59 -0300

    Get the icon for hiding

--- src/main/looset_graph/app.cljs
+++ src/main/looset_graph/app.cljs
@@ -540,20 +540,43 @@
       {:width "30" :height "30" :fill "currentColor" :viewBox "0 0 16 16"}
       [:path {:d "M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"}]]]]])
 
+(defn svg-eye
+  [props]
+  [:svg#eye.hover-gray
+   (merge
+     props
+     {:fill "currentColor" :viewBox "0 0 16 16" :xmlns "http://www.w3.org/2000/svg"})
+   [:path {:fill-rule "evenodd" :d "M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"}]
+   [:path {:fill-rule "evenodd" :d "M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"}]])
+
+(defn svg-filled-eye
+  [props]
+  [:svg#eye.hover-gray
+   (merge
+     props
+     {:fill "currentColor" :viewBox "0 0 16 16" :xmlns "http://www.w3.org/2000/svg"})
+   [:path {:fill-rule "evenodd" :d "M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"}]
+   [:path {:fill-rule "evenodd" :d "M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"}]])
+
 (defn node-view
   [{:keys [class]
     {:keys [color]} :style
     {:keys [level hidden? path node-id]} :node}
    text]
   [:div
-   {:style {:paddingLeft (+ 16 (* 12 level))}
+   {:style {:paddingLeft (+ 5 (* 12 level))}
     :class class
     :onMouseOver #(>evt [::node-hovered node-id])
     :onMouseOut #(>evt [::node-hovered nil])}
-   [:span.hover-gray
-    {:onClick #(>evt [::toggle-hidden node-id])
-     :style {:paddingRight 5}}
-    (if hidden? "🔲" "⬛")]
+   (if hidden?
+     [svg-eye
+      {:onClick #(>evt [::toggle-hidden node-id])
+       :style {:paddingRight 6}
+       :width "27" :height "27"}]
+     [svg-filled-eye
+      {:onClick #(>evt [::toggle-hidden node-id])
+       :style {:paddingRight 6}
+       :width "27" :height "27"}])
    [:div
     {:onClick #(>evt [::toggle-open-close path])
      :class (when (<sub [::foldable-node? node-id]) "hover-gray")
@@ -659,7 +682,8 @@
      flex-direction: row;
      font-family: "label-font-family", sans-serif;
      font-size: large;
-     padding-bottom: 13px;
+     padding-bottom: 10px;
+     align-items: center;
    }
 
    .label-style {
@@ -668,7 +692,8 @@
      font-family: "label-font-family", sans-serif;
      font-size: large;
      font-weight: bold;
-     padding-bottom: 13px;
+     padding-bottom: 10px;
+     align-items: center;
    }
    ")])
 
