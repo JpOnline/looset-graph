fa776b9faffac73cf2369e9fb19d2ce1db28b2a7
João Paulo <joao.paulo@vouch.io>
2025-03-15 06:40:30 -0300

    [improve] Change 'edit graph' button when it's active

--- src/main/looset_graph/app.cljs
+++ src/main/looset_graph/app.cljs
@@ -1137,9 +1137,12 @@
    {:style {:display "flex"
             :justify-content "space-evenly"
             :padding "10px"}}
-   [:button.button-1
+   [:button
     {:title "edit graph"
-     :onClick #(>evt [::toggle-edit-graph-text-area])}
+     :onClick #(>evt [::toggle-edit-graph-text-area])
+     :class (if @(re-frame/sub :flow {:id :f-editing-graph-text})
+              "button-1 active"
+              "button-1")}
     [:svg
      {:width "30" :height "30" :fill "currentColor" :viewBox "0 0 16 16" :xmlns "http://www.w3.org/2000/svg"}
      [:path {:fill-rule "evenodd" :d "M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"}]
@@ -1467,6 +1470,12 @@
    .button-1:hover,
    .button-1:focus {
      background-color: #7c7c7c;
+   }
+
+   .button-1.active {
+     background-color: #1b5f4b; /* Different color for active state */
+     color: #d1d1d1;
+     font-weight: 600; /* Slightly bolder text */
    }
 
    .button-2:active {
