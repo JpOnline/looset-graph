c67477cfc34be5eb69493e0d925d399f10abcca8
João Paulo <joao.paulo@vouch.io>
2025-03-18 05:16:14 -0300

    Add WIP button for select inverse

--- src/main/looset_graph/app.cljs
+++ src/main/looset_graph/app.cljs
@@ -1287,12 +1287,18 @@
        [:svg
         {:width icons-size :height icons-size :fill "currentColor" :viewBox "0 0 16 16"}
         [:path {:fill-rule "evenodd" :d "M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52z"}]]]
-      [:button.button-2.black-background
+      [:button.button-2
        {:title "select (shortcut: s)"
         :onClick #(>evt [::mouse-select-mode true])}
        [:svg
-        {:width icons-size :height icons-size :fill "#dddddd" :viewBox "0 0 16 16"}
+        {:width icons-size :height icons-size :fill "currentColor" :viewBox "0 0 16 16"}
         [:path {:fill-rule "evenodd" :d black-cursor-svg-path}]]]
+      ;; [:button.button-2.black-background
+      ;;  {:title "select inverse"
+      ;;   :onClick #(>evt [::mouse-select-mode true])}
+      ;;  [:svg
+      ;;   {:width icons-size :height icons-size :fill "#dddddd" :viewBox "0 0 16 16"}
+      ;;   [:path {:fill-rule "evenodd" :d black-cursor-svg-path}]]]
       [:div
        {:style {:height "2px"
                 :width "28px"
