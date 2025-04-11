1fc78e5990cd7f019bde5512c65cedc1aa7ddf6f
João Paulo <joao.paulo@vouch.io>
2025-04-11 15:41:52 -0300

    Better performance

--- js/cljs-runtime.old/shadow.module.client.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('client');
-
-try { looset_graph.app.init(); } catch (e) { console.error("An error occurred when calling (looset-graph.app/init)"); throw(e); }
--- js/cljs-runtime.old/shadow.module.frontend.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('frontend');
-
-try { looset_graph.app.init(); } catch (e) { console.error("An error occurred when calling (looset-graph.app/init)"); throw(e); }
