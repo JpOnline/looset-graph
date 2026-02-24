ac3a02f15beef662c1ad9a133bbae47a0900cff5
João Paulo <joao.paulo@vouch.io>
2026-02-24 15:04:45 -0300

    Add edge-string to graph-text
    - [bug] Quando carrego pelo graph do URL, os edge-strings não aparecem. Isso acontece pq a função nodes-map->graph-text-reduce-step não tá considerando os edge-strings;
    - Also add a button to change app-mode (from :trace to :graph).

--- src/main/looset_graph/app.cljs
+++ src/main/looset_graph/app.cljs
@@ -829,9 +829,17 @@
                             (-> s
                               (#(str "\""%"\""))
                               (#(if (= :label (:type (get nodes-map s {}))) (str "=>"%) %))))
+          flatten-rels (fn [edges-to]
+                         (for [[edge-string nodes-target] edges-to
+                               node-target nodes-target]
+                           [edge-string node-target]))
+          edge-st (fn [[edge-string _]]
+                    (if (= :nameless edge-string)
+                      "->"
+                      (str "-\""edge-string"\"->")))
           node-k (rename-if-label node-k*)
           node-children (seq (map rename-if-label (:children node-v)))
-          edges-to (->> node-v :edges-to vals (map vec) flatten (map rename-if-label) seq)
+          edges-to (->> node-v :edges-to flatten-rels (map #(update % 1 rename-if-label)))
           custom-props (dissoc node-v :type :edges-to :edges-from :label :children :foldable :parent)
           custom-props* (select-keys node-v [:name :position :hidden? :opened? :color])
           _ (assert (= custom-props custom-props*)
@@ -840,7 +848,7 @@
          (apply str (flatten (concat [children node-k ":\n"] (map #(str "  "%"\n") node-children) ["\n"])))
          children)
        (if edges-to
-         (apply str (flatten (concat [edges] (map #(str node-k" -> "%"\n") edges-to))))
+         (apply str (flatten (concat [edges] (map #(str node-k" "(edge-st %)" "(second %)"\n") edges-to))))
          edges)
        (if (seq custom-props)
          (str props node-k" "custom-props"\n")
@@ -984,6 +992,7 @@
       true (assoc-in [:db :ui :secret-buffer] next-buffer)
       secret-triggered? (-> (assoc-in [:db :ui :secret-buffer] [])
                             (assoc-in [:fx] [[:dispatch [::toggle-edit-graph-text-area]]
+                                             [:dispatch [::graph-app-mode]]
                                              [:dispatch [::toggle-super-user-options]]]))
       (= "v" keypressed) (assoc-in [:db :ui :mouse-select-mode] false)
       (= "s" keypressed) (assoc-in [:db :ui :mouse-select-mode] true)
@@ -1361,6 +1370,7 @@
   (re-frame/dispatch-sync [::fetch-markdown-explanation-content])
 
   (def app-state @re-frame.db/app-db)
+  (def graph-text (get-in app-state [:domain :graph-text]))
   (def visible-nodes-ids (get-in app-state [:ui :f-visible-nodes]))
   (def nodes-map (get-in app-state [:domain :nodes-map]))
   ; (find-visible visible-nodes-ids my-nodes-map "Computer_arithmetic")
@@ -1813,7 +1823,13 @@
       [:path {:d "M6.956 14.534c.065.936.952 1.659 1.908 1.42l.261-.065a1.378 1.378 0 0 0 1.012-.965c.22-.816.533-2.512.062-4.51.136.02.285.037.443.051.713.065 1.669.071 2.516-.211.518-.173.994-.68 1.2-1.272a1.896 1.896 0 0 0-.234-1.734c.058-.118.103-.242.138-.362.077-.27.113-.568.113-.856 0-.29-.036-.586-.113-.857a2.094 2.094 0 0 0-.16-.403c.169-.387.107-.82-.003-1.149a3.162 3.162 0 0 0-.488-.9c.054-.153.076-.313.076-.465a1.86 1.86 0 0 0-.253-.912C13.1.757 12.437.28 11.5.28H8c-.605 0-1.07.08-1.466.217a4.823 4.823 0 0 0-.97.485l-.048.029c-.504.308-.999.61-2.068.723C2.682 1.815 2 2.434 2 3.279v4c0 .851.685 1.433 1.357 1.616.849.232 1.574.787 2.132 1.41.56.626.914 1.28 1.039 1.638.199.575.356 1.54.428 2.591z"}]]
      [:svg
       {:width "30" :height "30" :fill "currentColor" :viewBox "0 0 16 16"}
-      [:path {:d "M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"}]]]]])
+      [:path {:d "M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"}]]]]
+   [:button.button-1
+    {:title "Toggle app-mode"
+     :onClick #(>evt [::toggle-app-mode])}
+    [:svg
+     {:width "30" :height "30" :fill "currentColor" :viewBox "0 0 16 16" :xmlns "http://www.w3.org/2000/svg"}
+     [:path {:fill-rule "evenodd" :d "M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098z"}]]]])
 
 (defn svg-eye
   [props]
@@ -2758,6 +2774,9 @@
 ;;     (.observe observer target-element #js{:attributes true :attributeFilter #js["style"]})))
 
 (re-frame/reg-sub ::app-mode (fn [app-state _] (get app-state :app-mode :trace)))
+(re-frame/reg-event-db ::graph-app-mode
+  (fn [app-state]
+    (assoc app-state :app-mode :graph)))
 (re-frame/reg-event-db ::toggle-app-mode
   (fn [app-state]
     (update app-state :app-mode #(if (= :trace %)
